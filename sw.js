0fdb76e9ccf510921bc3716aff04cea4b8a90df5
tknicus <rayhargg69@gmail.com>
2026-09-02 15:05:23 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,25 +1,20 @@
-const CACHE_NAME = 'mmc-scanner-v3.31'; // ⬅️ GI-UPDATE NATO ANG VERSION ARON MO-DOWNLOAD OG BAG-O
+const CACHE_NAME = 'mmc-scanner-v3.33'; // ⬅️ GI-UPDATE NATO ANG VERSION ARON MO-DOWNLOAD OG BAG-O
 
 // KINI ANG MGA FILES NGA I-DOWNLOAD UG I-SAVE SA SELPON INIG UNANG ABLI
 const urlsToCache = [
   './',
   './index.html',
   './database.js', // ⬅️ IDUGANG: Ang imong masterlist
-  './menu.html',
-  './menu.json',
-  './menu.png',
   './mmc.png', // ⬅️ IDUGANG: Ang watermark logo para sa picture
-  'https://lh3.googleusercontent.com/d/1m1NrFKOMKh4YjoUdXD0KvxcpySM5RuwU', // Ang imong banner image
   './scan.html',
   './scan.json',
   './scan.png',
-  'https://unpkg.com/html5-qrcode', // ⬅️ IDUGANG: Ang utok sa QR Scanner
-  './report.html',
   './map.html',
- 
-  // 👇 IDUGANG ANG IMONG KANTA DINHI 👇
+  './logo.png',
+  './tour-bg.jpg',
   './never_say_never.mp3', // ⬅️ IDUGANG: Ang para tugtog inig mag SEND sa DATA
-  //'./imong_kanta.mp3', // ⚠️ ILISI KINI KUNG UNSA GYUD ANG EXACT FILENAME SA IMONG AUDIO (ex: never_say_never.mp3)
+  'https://unpkg.com/html5-qrcode', // ⬅️ IDUGANG: Ang utok sa QR Scanner
+  'https://lh3.googleusercontent.com/d/1m1NrFKOMKh4YjoUdXD0KvxcpySM5RuwU', // Ang imong banner image
   'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
   'https://cdn.jsdelivr.net/npm/sweetalert2@8',
 ];
