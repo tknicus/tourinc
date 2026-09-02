1324b7b9e7dad0be3a86ce0cb0e5614f9f307b27
tknicus <rayhargg69@gmail.com>
2026-09-02 18:02:04 +0800

    Add files via upload

--- database.js
+++ database.js
@@ -6,9 +6,9 @@ const riderDatabase = [
   { idNum: "0017", fullname: "MCcoy", nickname: "17", level: "G5", category: "BMW GS450cc", classification: "MASTER", passcode: "fe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3", isMarshal: false },
   { idNum: "0251", fullname: "Zhack", nickname: "zhackyyy", level: "G1S", category: "Rouser RS2000", classification: "Aspirant", passcode: "e0799b5b526a8866b02243691e20c51cf33ef66d84c7c4e3b99727aa48d21d20", isMarshal: false },
   { idNum: "1605", fullname: "Jabla, Joshua ", nickname: "JOSHUA", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "5492dcdcf24546ce707e4c6e370ab4a0736d0defa0cbbf6f66798e57a309c6a1", isMarshal: false },
-  { idNum: "1609", fullname: "PADRONES, ROCKY", nickname: "RaKiE", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "", isMarshal: false },
+  { idNum: "1609", fullname: "PADRONES, ROCKY", nickname: "RaKiE", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "e40e0225f947f1ae6cba2245dd9a1a50361512bb718c1ca1ce3f983fe3b3aa4e", isMarshal: false },
   { idNum: "1617", fullname: "REMOLLENO, ALEXANDER ", nickname: "XRM SAKALAM", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "", isMarshal: false },
   { idNum: "1634", fullname: "Galvadores, Riche Val", nickname: "Long Way PH", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "", isMarshal: false },
   { idNum: "1635", fullname: "German, Henry Gulle", nickname: "technicus", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "", isMarshal: false },
   // Dinhi nimo isumpay ang ubang riders...
-];
+]
