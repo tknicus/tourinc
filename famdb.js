b3f9a729dbe79885ed8a36628d031b8ef22d5e02
tknicus <rayhargg69@gmail.com>
2026-09-06 18:28:13 +0800

    Add files via upload

--- famdb.js
+++ famdb.js
@@ -1,4 +1,5 @@
 const familyDatabase = [
     { idNum: "01", fullname: "technicus", route: "Balay" },
     { idNum: "02", fullname: "kHEr", route: "Laag" },
+    { idNum: "19", fullname: "pRenz", route: "Luzon" },
 ];
