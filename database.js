648c97c469ca5285f12b7b9ad39829bd1fcd1705
tknicus <rayhargg69@gmail.com>
2026-09-12 23:01:33 +0800

    Add files via upload

--- database.js
+++ database.js
@@ -4,16 +4,16 @@
 
 // 1. RIDER MASTERLIST (Mga ID, pangalan, ug Hashed Passcodes)
 const riderDatabase = [
-    { idNum: "0000", fullname: "ELKAPITAN", nickname: "invictus", level: "Street Pilot", category: "Adventure 500cc", classification: "Street Pilot", passcode: "a045f58fe4162bb9227e93f549390aef6219914df85029f74f6057c3fda6ca85", isMarshal: true },
-    { idNum: "0001", fullname: "technicus", nickname: "rayHARGG", level: "AS26", category: "Adventure 525cc", classification: "Aspirant", passcode: "4c327801d652b4e7b3449684f83febb4fea62dbd2c80ee4136f32ecbccc0f45e", isMarshal: true },
-    { idNum: "0002", fullname: "Long Way Ph", nickname: "Elkapitan Jr", level: "AS26", category: "ADV 350cc", classification: "MASTER", passcode: "e40e0225f947f1ae6cba2245dd9a1a50361512bb718c1ca1ce3f983fe3b3aa4e", isMarshal: true },
-    { idNum: "0017", fullname: "MCcoy", nickname: "17", level: "G5", category: "BMW GS450cc", classification: "MASTER", passcode: "fe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3", isMarshal: false },
-    { idNum: "0251", fullname: "Zhack", nickname: "zhackyyy", level: "G1S", category: "Rouser RS2000", classification: "Aspirant", passcode: "e0799b5b526a8866b02243691e20c51cf33ef66d84c7c4e3b99727aa48d21d20", isMarshal: false },
-    { idNum: "1605", fullname: "Jabla, Joshua ", nickname: "JOSHUA", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "5492dcdcf24546ce707e4c6e370ab4a0736d0defa0cbbf6f66798e57a309c6a1", isMarshal: false },
-    { idNum: "1609", fullname: "PADRONES, ROCKY", nickname: "RaKiE", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "e40e0225f947f1ae6cba2245dd9a1a50361512bb718c1ca1ce3f983fe3b3aa4e", isMarshal: false },
-    { idNum: "1617", fullname: "REMOLLENO, ALEXANDER ", nickname: "XRM SAKALAM", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "9f2ca3ad31656671b9e0bf12509c9e71dac5b8e8834145767f684bf193945d33", isMarshal: false },
-    { idNum: "1634", fullname: "Galvadores, Riche Val", nickname: "Long Way PH", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "0", isMarshal: false },
-    { idNum: "1635", fullname: "German, Henry Gulle", nickname: "technicus", level: "AS26", category: "LONEWOLF", classification: "MASTER", passcode: "0", isMarshal: false }
+    { idNum: "0000", fullname: "ELKAPITAN", nickname: "invictus", level: "Street Pilot", category: "Adventure", classification: "Street Pilot", vehicle: "Bristol 500", passcode: "a045f58fe4162bb9227e93f549390aef6219914df85029f74f6057c3fda6ca85", isMarshal: true },
+    { idNum: "0001", fullname: "technicus", nickname: "rayHARGG", level: "AS26", category: "Touring", classification: "Aspirant", vehicle: "Rouser NS200", passcode: "4c327801d652b4e7b3449684f83febb4fea62dbd2c80ee4136f32ecbccc0f45e", isMarshal: true },
+    { idNum: "0002", fullname: "Long Way Ph", nickname: "Elkapitan Jr", level: "AS26", category: "Adeventure", classification: "MASTER", vehicle: "Yamaha ADV350", passcode: "e40e0225f947f1ae6cba2245dd9a1a50361512bb718c1ca1ce3f983fe3b3aa4e", isMarshal: true },
+    { idNum: "0017", fullname: "MCcoy", nickname: "17", level: "G5", category: "Adventure", classification: "MASTER", vehicle: "BMW GS450", passcode: "fe2592b42a727e977f055947385b709cc82b16b9a87f88c6abf3900d65d0cdc3", isMarshal: false },
+    { idNum: "0251", fullname: "Zhack", nickname: "zhackyyy", level: "G1S", category: "Racing", classification: "Aspirant", vehicle: "Rouser RS200", passcode: "e0799b5b526a8866b02243691e20c51cf33ef66d84c7c4e3b99727aa48d21d20", isMarshal: false },
+    { idNum: "1605", fullname: "Jabla, Joshua ", nickname: "JOSHUA", level: "AS26", category: "LONEWOLF", classification: "MASTER", vehicle: "", passcode: "5492dcdcf24546ce707e4c6e370ab4a0736d0defa0cbbf6f66798e57a309c6a1", isMarshal: false },
+    { idNum: "1609", fullname: "PADRONES, ROCKY", nickname: "RaKiE", level: "AS26", category: "LONEWOLF", classification: "MASTER", vehicle: "", passcode: "e40e0225f947f1ae6cba2245dd9a1a50361512bb718c1ca1ce3f983fe3b3aa4e", isMarshal: false },
+    { idNum: "1617", fullname: "REMOLLENO, ALEXANDER ", nickname: "XRM SAKALAM", level: "AS26", category: "LONEWOLF", classification: "MASTER", vehicle: "", passcode: "9f2ca3ad31656671b9e0bf12509c9e71dac5b8e8834145767f684bf193945d33", isMarshal: false },
+    { idNum: "1634", fullname: "Galvadores, Riche Val", nickname: "Long Way PH", level: "AS26", category: "LONEWOLF", classification: "MASTER", vehicle: "", passcode: "0", isMarshal: false },
+    { idNum: "1635", fullname: "German, Henry Gulle", nickname: "technicus", level: "AS26", category: "LONEWOLF", classification: "MASTER", vehicle: "", passcode: "0", isMarshal: false }
 ];
 
 
