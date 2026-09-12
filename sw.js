9de692bc8ce5e6f598067a6e582a593206ff470d
tknicus <rayhargg69@gmail.com>
2026-09-12 18:16:21 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,23 @@
 const CACHE_NAME = 'mmc-scanner-v3.40'; // ⬅️ GI-UPDATE NATO ANG VERSION ARON MO-DOWNLOAD OG BAG-O
 
 // KINI ANG MGA FILES NGA I-DOWNLOAD UG I-SAVE SA SELPON INIG UNANG ABLI
-const urlsToCache = [
-  './',
-  './index.html',
-  './database.js', // ⬅️ IDUGANG: Ang imong masterlist
-  './mmc.png', // ⬅️ IDUGANG: Ang watermark logo para sa picture
-  './scan.html',
-  './scan.json',
-  './scan.png',
-  './map.html',
-  './logo.png',
-  './tour-bg.jpg',
-  './never_say_never.mp3', // ⬅️ IDUGANG: Ang para tugtog inig mag SEND sa DATA
-  'https://unpkg.com/html5-qrcode', // ⬅️ IDUGANG: Ang utok sa QR Scanner
-  'https://lh3.googleusercontent.com/d/1m1NrFKOMKh4YjoUdXD0KvxcpySM5RuwU', // Ang imong banner image
-  'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
-  'https://cdn.jsdelivr.net/npm/sweetalert2@8',
+const assetsToCache = [
+    './',
+    './index.html',
+    './database.js', // ⬅️ IDUGANG: Ang imong masterlist
+    './mmc.png', // ⬅️ IDUGANG: Ang watermark logo para sa picture
+    './scan.html',
+    './scan.json',
+    './scan.png',
+    './map.html',
+    './logo.png',
+    './tour-bg.jpg',
+    './never_say_never.mp3', // ⬅️ IDUGANG: Ang para tugtog inig mag SEND sa DATA
+    'https://unpkg.com/html5-qrcode', // ⬅️ IDUGANG: Ang utok sa QR Scanner
+    'https://lh3.googleusercontent.com/d/1m1NrFKOMKh4YjoUdXD0KvxcpySM5RuwU', // Ang imong banner image
+    'https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css',
+    'https://cdn.jsdelivr.net/npm/sweetalert2@8',
 ];
-
 
 // 1. I-install ug i-cache ang mga files
 self.addEventListener('install', (event) => {
